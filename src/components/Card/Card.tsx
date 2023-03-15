187d2d746c13e743d2a45c6f4fc381401cc968bd
Anton Zhytonbaiev <chipchilinka11@gmail.com>
2023-03-15 19:59:12 +0200

    some fixes

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -14,61 +14,38 @@ export const Card: React.FC = () => {
       </h1>
 
       <div className="card__price">
-        <p className="card__price-new">
-          $799
-        </p>
+        <p className="card__price-new">$799</p>
 
-        <p className="card__price-old">
-          $999
-        </p>
+        <p className="card__price-old">$999</p>
       </div>
 
       <div className="card__line"></div>
 
       <div className="card__characteristics">
         <div className="card__char">
-          <p className="card__char-text">
-            Screen
-          </p>
+          <p className="card__char-text">Screen</p>
 
-          <p className="card__char-number">
-            6.5” OLED
-          </p>
+          <p className="card__char-number">6.5” OLED</p>
         </div>
 
         <div className="card__char">
-          <p className="card__char-text">
-            Capacity
-          </p>
+          <p className="card__char-text">Capacity</p>
 
-          <p className="card__char-number">
-            64 GB
-          </p>
+          <p className="card__char-number">64 GB</p>
         </div>
 
         <div className="card__char">
-          <p className="card__char-text">
-            RAM
-          </p>
+          <p className="card__char-text">RAM</p>
 
-          <p className="card__char-number">
-            4 GB
-          </p>
+          <p className="card__char-number">4 GB</p>
         </div>
       </div>
 
       <div className="card__buttons">
-        <a
-          href="#AddToCart"
-          className="card__buttons-addCart"
-        >
+        <a href="#AddToCart" className="card__buttons-addCart">
           Add to cart
         </a>
-        <a
-          href="#AddToList"
-          className="card__buttons-addList"
-        >
-        </a>
+        <a href="#AddToList" className="card__buttons-addList"></a>
       </div>
     </article>
   );
