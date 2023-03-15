f1c7d4bf27656065782c05931d64e70fe705e250
Andrew Borys <borysandrey1988@gmail.com>
2023-03-15 20:04:00 +0200

    initial footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,33 +1,69 @@
 import React from 'react';
 import styles from './Footer.module.scss';
+import arrowIcon from './img/Vector.svg';
+import logo from './img/Logo.svg';
 
 export const Footer: React.FC = () => {
+  const handleClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+
+    console.log('to top');
+  };
+
   return (
-    <footer>
-      <div>
-        <img src="" alt="" />
-      </div>
-      <ul>
-        <li className={styles.footer_link}>
-          <a
-            href="https://github.com/orgs/fe-nov22-WardNo6/repositories"
-            target="_blank"
-            rel="noreferrer"
+    <footer className={styles.footer}>
+      <div className={styles.footer__container}>
+        <div className={styles.footer__logo}>
+          <img src={logo} alt="Logo" />
+        </div>
+        <ul className={styles.footer__list}>
+          <li className={styles.footer__list_item}>
+            <a
+              className={styles.footer__list_link}
+              href="https://github.com/orgs/fe-nov22-WardNo6/repositories"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </li>
+          <li className={styles.footer__list_item}>
+            <a
+              className={styles.footer__list_link}
+              href="#"
+            >
+              Contacts
+            </a>
+          </li>
+          <li className={styles.footer__list_item}>
+            <a
+              className={styles.footer__list_link}
+              href="#"
+            >
+              Rights
+            </a>
+          </li>
+        </ul>
+        <div className={styles.footer__top}>
+          <span className={styles.footer__top__text}>
+            Back to top
+          </span>
+          <div
+            className={styles.footer__top__button}
+            onClick={handleClick}
           >
-            Github
-          </a>
-        </li>
-        <li className={styles.footer_link}>
-          <a href="#">Contacts</a>
-        </li>
-        <li className={styles.footer_link}>
-          <a href="#">Rights</a>
-        </li>
-      </ul>
-      <div>
-        <span className={styles.footer__toTopText}>Back to top</span>
-        <div>^</div>
+            <img
+              className={styles.arrowIcon}
+              src={arrowIcon}
+              alt="to top"
+            />
+          </div>
+        </div>
       </div>
+
     </footer>
   );
 };
