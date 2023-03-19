1b6dbbd53858723ab338760b9fcc0c5dceb24c13
Artem_Sobolevskiy <artemsobolevskuy@gmail.com>
2023-03-19 20:37:04 +0200

    pageNotFound was added

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import pageNotFoundIcon from '../../icons/pageNotFound.svg';
+import './PageNotFound.scss';
 
 export const PageNotFound = () => {
+  const navigate = useNavigate();
+  const [seconds, setSeconds] = useState(5);
+  const timerId = setInterval(() => setSeconds(seconds - 1), 1000);
+
+  useEffect(() => {
+    setTimeout(() => { clearInterval(timerId); navigate('/');}, 5000);
+  }, []);
+
   return (
-    <div>
-      <h1>404</h1>
-      <h1>Page not found</h1>
+    <div className='notFound'>
+      <img src={pageNotFoundIcon} className='notFound__icon'></img>
+      <h2 className='notFound__title'>{`You will be redirected to Home page after ${seconds} seconds...`}</h2>
     </div>
   );
 };
