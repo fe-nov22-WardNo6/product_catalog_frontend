72e78931c1dbeadbb30a6cb3da9f79bd4edae95c
Artem_Sobolevskiy <artemsobolevskuy@gmail.com>
2023-03-20 00:17:29 +0200

    burger menu was added

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -1,5 +1,72 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import cn from 'classnames';
+import cart_icon from '../../icons/cart.svg';
+import favorites_icon from '../../icons/heart.svg';
+import './Burger.scss';
 
 export const Burger = () => {
-  return <h1>Test</h1>;
+  return (
+    <div className='burger'>
+      <div className='burger__content'>
+        <div className="burger__buttons">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              cn('burger__button', { 'is-active': isActive })
+            }
+          >
+              HOME
+          </NavLink>
+          <NavLink
+            to="/phones"
+            className={({ isActive }) =>
+              cn('burger__button', { 'is-active': isActive })
+            }
+          >
+              PHONES
+          </NavLink>
+          <NavLink
+            to="/tablets"
+            className={({ isActive }) =>
+              cn('burger__button', { 'is-active': isActive })
+            }
+          >
+              TABLETS
+          </NavLink>
+          <NavLink
+            to="/accessories"
+            className={({ isActive }) =>
+              cn('burger__button', { 'is-active': isActive })
+            }
+          >
+              ACCESSORIES
+          </NavLink>
+        </div>
+        <div className="burger__actions">
+          <NavLink
+            to="/favorites"
+            className={({ isActive }) =>
+              cn('burger__action', { 'is-active': isActive })
+            }
+          >
+            <img
+              src={favorites_icon}
+              alt="Nice Gadgets logo"
+              className="icon"
+            />
+          </NavLink>
+
+          <NavLink
+            to="/cart"
+            className={({ isActive }) =>
+              cn('burger__action', { 'is-active': isActive })
+            }
+          >
+            <img src={cart_icon} alt="Nice Gadgets logo" className="icon" />
+          </NavLink>
+        </div>
+      </div>
+    </div>    
+  );
 };
