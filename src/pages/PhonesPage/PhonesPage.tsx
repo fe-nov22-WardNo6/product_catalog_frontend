2fb5d972e055db69dc23586d403d21fd32f85f97
Andrew Borys <borysandrey1988@gmail.com>
2023-03-20 15:46:08 +0200

    fixed footer & SortPanel

--- src/pages/PhonesPage/PhonesPage.tsx
+++ src/pages/PhonesPage/PhonesPage.tsx
@@ -5,6 +5,7 @@ import '../../style/grid.scss';
 import { Card } from '../../components/Card';
 import { Phone } from '../../types/PhoneDefault';
 import { getPhones } from '../../api/api';
+import { SortPanel } from '../../components/SortPanel';
 
 export const PhonesPage: React.FC = () => {
   const [phones, setPhones] = useState<Phone[]>([]);
@@ -44,62 +45,67 @@ export const PhonesPage: React.FC = () => {
   }, []);
 
   return (
-    <div className="phonesPage container">
-      {/* <p className="text">page navigation</p>
-      <h1 className="phones-page__title">Mobile phones</h1>
-      <p className="phones-page__countPhones">95 models</p>
-      <div className="phones-page__displayOptions">component with form</div> */}
-      {isDataLoading && 'loading data'}
-      {!isDataLoading && !isError && (
-        <div className="phones-page__phones-container grid grid--desktop grid--tablet grid--landscape">
-          {phones.map((phone, i) => {
+    <>
+      <SortPanel
+        getPhones={getPhonesFromServer}
+      />
+      <div className="phonesPage container">
+        {/* <p className="text">page navigation</p>
+    <h1 className="phones-page__title">Mobile phones</h1>
+    <p className="phones-page__countPhones">95 models</p>
+    <div className="phones-page__displayOptions">component with form</div> */}
+        {isDataLoading && 'loading data'}
+        {!isDataLoading && !isError && (
+          <div className="phones-page__phones-container grid grid--desktop grid--tablet grid--landscape">
+            {phones.map((phone, i) => {
 
-            const gridClassArray = [];
-            if (width > 1199) {
-              if ((i + 1) % 4 === 3) {
-                gridClassArray.push('grid__item-desktop--13-18');
+              const gridClassArray = [];
+              if (width > 1199) {
+                if ((i + 1) % 4 === 3) {
+                  gridClassArray.push('grid__item-desktop--13-18');
+                }
+                if ((i + 1) % 4 === 2) {
+                  gridClassArray.push('grid__item-desktop--7-12');
+                }
+                if ((i + 1) % 4 === 1) {
+                  gridClassArray.push('grid__item-desktop--1-6');
+                }
+                if ((i + 1) % 4 === 0) {
+                  gridClassArray.push('grid__item-desktop--19-24');
+                }
               }
-              if ((i + 1) % 4 === 2) {
-                gridClassArray.push('grid__item-desktop--7-12');
-              }
-              if ((i + 1) % 4 === 1) {
-                gridClassArray.push('grid__item-desktop--1-6');
-              }
-              if ((i + 1) % 4 === 0) {
-                gridClassArray.push('grid__item-desktop--19-24');
-              }
-            }
 
-            if (width <= 1199 && width > 768) {
-              if ((i + 1) % 3 === 2) {
-                gridClassArray.push('grid__item-landscape--5-8');
+              if (width <= 1199 && width > 768) {
+                if ((i + 1) % 3 === 2) {
+                  gridClassArray.push('grid__item-landscape--5-8');
+                }
+                if ((i + 1) % 3 === 1) {
+                  gridClassArray.push('grid__item-landscape--1-4');
+                }
+                if ((i + 1) % 3 === 0) {
+                  gridClassArray.push('grid__item-landscape--9-12');
+                }
               }
-              if ((i + 1) % 3 === 1) {
-                gridClassArray.push('grid__item-landscape--1-4');
-              }
-              if ((i + 1) % 3 === 0) {
-                gridClassArray.push('grid__item-landscape--9-12');
-              }
-            }
 
-            if (width <= 768 && width > 639) {
-              if ((i + 1) % 2 !== 0) {
-                gridClassArray.push('grid__item-tablet--1-6');
+              if (width <= 768 && width > 639) {
+                if ((i + 1) % 2 !== 0) {
+                  gridClassArray.push('grid__item-tablet--1-6');
+                }
+                if ((i + 1) % 2 === 0) {
+                  gridClassArray.push('grid__item-tablet--7-12');
+                }
               }
-              if ((i + 1) % 2 === 0) {
-                gridClassArray.push('grid__item-tablet--7-12');
-              }
-            }
 
-            const gridClass = gridClassArray.join(' ');
+              const gridClass = gridClassArray.join(' ');
 
-            return (
-              <Card key={phone.phoneId} phone={phone} gridClass={gridClass} />
-            );
-          })}
-        </div>
-      )}
-      {isError && 'not found'}
-    </div>
+              return (
+                <Card key={phone.phoneId} phone={phone} gridClass={gridClass} />
+              );
+            })}
+          </div>
+        )}
+        {isError && 'not found'}
+      </div>
+    </>
   );
 };
