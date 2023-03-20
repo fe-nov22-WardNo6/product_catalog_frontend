b4edbbd86cc9e4cf2f0f1c5bd1471a71f6af9e2a
PolinaSukhorukova <sukhorukovapolina03@gmail.com>
2023-03-21 00:29:11 +0200

    add button was changed

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const HomePage: React.FC = () => {
   return (
     <>
-      <h1 style={{ color: 'white', height: '100vh' }}>Home page</h1>
+      <h1 style={{ color: 'white' }} className="container" >Home page</h1>
     </>
   );
 };
