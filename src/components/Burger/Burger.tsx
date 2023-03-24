20a2011ceaa67cc9d603e3b316131465563a0d18
PolinaSukhorukova <sukhorukovapolina03@gmail.com>
2023-03-24 13:53:33 +0200

    Burger counter added

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import cn from 'classnames';
 import cart_icon from '../../icons/cart.svg';
 import favorites_icon from '../../icons/heart.svg';
 import './Burger.scss';
+import { ActionContext } from '../../context/ActionContext';
 
 type Props = {
   setIsBurgerActive: (boolean: boolean) => void;
 };
 
 export const Burger: React.FC<Props> = ({ setIsBurgerActive }) => {
+  const { cartItems, favoritesItems } = useContext(ActionContext);
+  const countArrCart = cartItems.map((el) => el.count);
+  const countSumCart = getSum(countArrCart);
+  const countArrFav = favoritesItems.map((el) => el.count);
+  const countSumFav = getSum(countArrFav);
+
+  function getSum(arr: number[]) {
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+
+    return sum;
+  }
+
   return (
     <div className="burger">
       <div className="burger__content">
@@ -64,6 +81,10 @@ export const Burger: React.FC<Props> = ({ setIsBurgerActive }) => {
               alt="Nice Gadgets logo"
               className="icon"
             />
+
+            {countSumFav > 0 && (
+              <div className="burger__action-counter">{countSumFav}</div>
+            )}
           </NavLink>
 
           <NavLink
@@ -74,6 +95,9 @@ export const Burger: React.FC<Props> = ({ setIsBurgerActive }) => {
             onClick={() => setIsBurgerActive(false)}
           >
             <img src={cart_icon} alt="Nice Gadgets logo" className="icon" />
+            {countSumCart > 0 && (
+              <div className="burger__action-counter">{countSumCart}</div>
+            )}
           </NavLink>
         </div>
       </div>
